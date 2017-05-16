2cffff4fd1a2af1d020257e6775fdc8d4371c988
TomaszWieja <tomasz.wieja@o2.pl>
2017-05-16 20:53:30 +0200

    Database connection

---
+++ api/src/connection.php
@@ -0,0 +1,12 @@
+<?php
+
+$baseName = "Library_app";
+$serverName = "localhost";
+$userName = "root";
+$password = "coderslab";
+
+$conn = new mysqli($serverName, $userName, $password, $baseName);
+
+if ($conn->connect_error) {
+    die("Blad: " . $conn->connect_error);
+}
